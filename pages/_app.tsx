95132d1d33113235893f48157a0f42ad252b1ce8
przemek <przemek.kalisiewicz@purplebricks.com>
2021-01-08 23:49:37 +0000

    add react-axe
    Change log:
    - added React-Axe which will determine if there are any accessibility violations in code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import App from 'next/app'
 import { ThemeProvider } from 'styled-components'
 
 import { defaultTheme } from '@tooploox-theme'
+import { hasWindow } from '@tooploox-helpers'
 
+const isLocalEnv = process.env.NODE_ENV === 'development'
+
+if (isLocalEnv && !hasWindow()) {
+  const axe = require('react-axe')
+  axe(React, ReactDOM, 1000)
+}
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props
