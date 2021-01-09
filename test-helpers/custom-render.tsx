f0a4110cbc3740fee9e4fe7b4e12f136895a0dc4
przemek <przemek.kalisiewicz@purplebricks.com>
2021-01-09 17:41:24 +0000

    fix custom render
    Change log:
    - fixed custom render function to use the defaultTheme

--- test-helpers/custom-render.tsx
+++ test-helpers/custom-render.tsx
@@ -2,8 +2,10 @@ import { PropsWithChildren, ReactElement } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { render, RenderResult, RenderOptions } from '@testing-library/react'
 
+import { defaultTheme } from '@tooploox-test/theme'
+
 function DefaultProviders({ children }: PropsWithChildren<{}>): ReactElement {
-  return <ThemeProvider theme="default">{children}</ThemeProvider>
+  return <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>
 }
 
 function customRenderWrapper(
