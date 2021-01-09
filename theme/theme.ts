31f4c8f0607a49579d80a221c7eb9cb0920b5d02
przemek <przemek.kalisiewicz@purplebricks.com>
2021-01-09 11:51:38 +0000

    set up init colors
    Change log:
    - added entry styles taken from figma design

--- theme/theme.ts
+++ theme/theme.ts
@@ -1,5 +1,27 @@
 import { DefaultTheme } from 'styled-components'
 
-const defaultTheme: DefaultTheme = {}
+const defaultTheme: DefaultTheme = {
+  colors: {
+    black: '#000000',
+    blue: '#2F80ED',
+    grey1: '#F0F3F4',
+    grey2: '#E5E5E5',
+    grey3: '#828282',
+    grey4: '#9E9E9E',
+    navy: '#452CDC',
+    white: '#FFFFFF',
+  },
+  font: {
+    family: {
+      primary: 'Roboto',
+    },
+  },
+  radius: {
+    sm: '0.4rem',
+    md: '0.8rem',
+    lg: '1.2rem',
+    xl: '1.6rem',
+  },
+}
 
 export { defaultTheme }
