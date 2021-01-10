c9280f719e5bb8e655201b8e0df38364a660dd44
przemek <przemek.kalisiewicz@purplebricks.com>
2021-01-10 11:53:20 +0000

    standardise properties destructuring
    - standardised button atom props destructuring to match standards

--- components/atoms/button/button.tsx
+++ components/atoms/button/button.tsx
@@ -3,6 +3,6 @@ import React, { ReactElement } from 'react'
 import { StyledButton } from './button.styled'
 import { TButtonProperties } from './button.types'
 
-export function Button({ ...properties }: TButtonProperties): ReactElement {
+export function Button(properties: TButtonProperties): ReactElement {
   return <StyledButton {...properties} />
 }
