7084eba37ac4226b7206ef45b33626b05d1f1c52
przemek <przemek.kalisiewicz@purplebricks.com>
2021-01-12 16:21:17 +0000

    remove search form due to abstraction needed
    Change log:
    - removed form molecule as it was a very small molecule
    There was no benefits of having it abstracted as well as it was requiring prop drilling.

--- components/molecules/search-form/search-form.styled.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-export const StyledFormWrapper = styled.form`
-  display: grid;
-  grid-template-columns: minmax(24.5rem, 2fr) minmax(6.4rem, 15rem);
-  grid-gap: 1.6rem;
-`
