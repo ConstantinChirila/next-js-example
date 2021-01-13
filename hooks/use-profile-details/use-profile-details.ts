76958365347d73ea533be3834b9621316151b5cd
przemek <przemek.kalisiewicz@purplebricks.com>
2021-01-13 13:28:40 +0000

    rename query-keys and add dependency to use-repositories hook
    Change log:
    - renamed query-keys to match to the content of hook
    - set use-repositories hook enabled only if profile exists

--- hooks/use-profile-details/use-profile-details.ts
+++ hooks/use-profile-details/use-profile-details.ts
@@ -3,5 +3,7 @@ import { useQuery } from 'react-query'
 import { getUserDetails } from '@tooploox-test/services'
 
 export function useProfileDetails(name: string) {
-  return useQuery(['name', name], () => getUserDetails(name))
+  return useQuery(['profile', name], () => getUserDetails(name), {
+    enabled: !!name,
+  })
 }
--- hooks/use-repositories/use-repositories.ts
+++ hooks/use-repositories/use-repositories.ts
@@ -2,6 +2,12 @@ import { useQuery } from 'react-query'
 
 import { getUserRepositories } from '@tooploox-test/services'
 
-export function useRepositories(name: string) {
-  return useQuery(['name', name], () => getUserRepositories(name))
+export function useRepositories(name: string, isFetchEnabled: boolean) {
+  return useQuery(
+    ['repositories', name && name],
+    () => getUserRepositories(name),
+    {
+      enabled: isFetchEnabled,
+    }
+  )
 }
