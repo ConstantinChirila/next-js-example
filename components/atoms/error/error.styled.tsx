c53a67748da5f16f70a7f201219d4daea5a0bce6
przemek <przemek.kalisiewicz@purplebricks.com>
2021-01-13 19:35:11 +0000

    fix import for error styled and add exports for types and styles

--- components/atoms/error/error.styled.tsx
+++ components/atoms/error/error.styled.tsx
@@ -14,8 +14,12 @@ export const StyledErrorMessage = styled.p`
   line-height: 1.6;
 `
 
-export const StyledNotFoundIcon = styled(IconNotFound)`
-  fill: ${({ theme }) => theme.colors.white};
-  height: 7rem;
-  width: 7rem;
+export const StyledNotFoundIcon = styled((properties) => (
+  <IconNotFound {...properties} />
+))`
+  && {
+    fill: ${({ theme }) => theme.colors.white};
+    height: 7rem;
+    width: 7rem;
+  }
 `
--- components/atoms/error/index.ts
+++ components/atoms/error/index.ts
@@ -1,1 +1,3 @@
 export * from './error'
+export * from './error.styled'
+export * from './error.types'
