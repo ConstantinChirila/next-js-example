1154e1f879d1876a89a925275d6bd4a397526bfd
przemek <przemek.kalisiewicz@purplebricks.com>
2021-01-14 14:30:16 +0000

    set up handler server for the happy path and add first happy path test
    Change log:
    - set up handler so it can intercept API requests and give responses based on test
    - added first page test to determine if user profile and repositories are showing correctly

--- pages-lib/index.spec.tsx
+++ pages-lib/index.spec.tsx
@@ -1,8 +1,35 @@
 import React from 'react'
-import { screen } from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
 
 import IndexPage from '../pages'
 import { renderWithReactQueryContext } from '../test-helpers'
+import { urls } from '../configuration'
+import { githubResponseWithUser, gitubResponseWithRepositories } from '../mocks'
+
+// default happy path handlers
+const handlers = [
+  rest.get(
+    `${urls.githubUsersApi}/kentcdodds`,
+    (_request, response, context) => {
+      return response(context.json(githubResponseWithUser))
+    }
+  ),
+  rest.get(
+    `${urls.githubUsersApi}/kentcdodds/repos*`,
+    (_request, response, context) => {
+      return response(context.json(gitubResponseWithRepositories))
+    }
+  ),
+]
+
+// default server with the correct responses
+const server = setupServer(...handlers)
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
 
 it('should show only the form with a hint by default', async () => {
   renderWithReactQueryContext(<IndexPage />)
@@ -19,4 +46,61 @@ it('should show only the form with a hint by default', async () => {
   expect(
     screen.getByText(/please search for any user e.g. tannerlinsley/i)
   ).toBeInTheDocument()
+})
+
+it('should render user profile with repositories when correct response', async () => {
+  renderWithReactQueryContext(<IndexPage />)
+
+  userEvent.type(
+    screen.getByRole('textbox', {
+      name: /search for users/i,
+    }),
+    'kentcdodds'
+  )
+  userEvent.click(
+    screen.getByRole('button', {
+      name: /search/i,
+    })
+  )
+
+  await waitFor(() =>
+    expect(
+      screen.getByRole('img', {
+        name: /avatar of kent c\. dodds/i,
+      })
+    ).toBeInTheDocument()
+  )
+
+  expect(
+    screen.getByRole('heading', {
+      name: /kent c\. dodds/i,
+    })
+  ).toBeInTheDocument()
+  expect(screen.getByText(/lovely bio from kentcdodds/i)).toBeInTheDocument()
+
+  await waitFor(() =>
+    expect(
+      screen.getByRole('heading', {
+        name: /top repositories/i,
+      })
+    ).toBeInTheDocument()
+  )
+
+  expect(
+    screen.getByRole('link', {
+      name: /most-starred-repo/i,
+    })
+  ).toBeInTheDocument()
+
+  expect(
+    screen.getByRole('link', {
+      name: /sort-of-accepted-library/i,
+    })
+  ).toBeInTheDocument()
+
+  expect(
+    screen.queryByRole('link', {
+      name: /most-hated-library/i,
+    })
+  ).not.toBeInTheDocument()
 })
