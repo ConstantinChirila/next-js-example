4baf148d1a82d9c4413b51254701e4ee6a5681a2
przemek <przemek.kalisiewicz@purplebricks.com>
2021-01-14 18:28:56 +0000

    comment out react-axe to decrease the bundle size

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,15 +5,20 @@ import { QueryClientProvider, QueryClient } from 'react-query'
 
 import { defaultTheme, GlobalStyles } from '@tooploox-test/theme'
 import { reactQueryDefaults } from '@tooploox-test/configuration'
-import { hasWindow } from '@tooploox-test/helpers'
 
-const isLocalEnvironment = process.env.NODE_ENV === 'development'
+/*
+  This is commented out as for some reason Next.js bundles this code when running on SSG mode.
+  I will raise a bug on the Next.js side in a few days.
+  If you want to check for the Axe errors, simply uncomment it out.
+*/
 
-if (isLocalEnvironment && hasWindow()) {
-  const ReactDOM = require('react-dom')
-  const axe = require('@axe-core/react')
-  axe(React, ReactDOM, 1000)
-}
+// import { hasWindow } from '@tooploox-test/helpers'
+// const isLocalEnvironment = process.env.NODE_ENV === 'development'
+// if (isLocalEnvironment && hasWindow()) {
+//   const ReactDOM = require('react-dom')
+//   const axe = require('@axe-core/react')
+//   axe(React, ReactDOM, 1000)
+// }
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props
