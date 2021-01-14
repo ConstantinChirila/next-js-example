aa26aab1ce04950f1f2a89f0f44a28b8779b0931
przemek <przemek.kalisiewicz@purplebricks.com>
2021-01-14 11:40:12 +0000

    create custom renderer to include react-query provider in tests

--- test-helpers/custom-render.tsx
+++ test-helpers/custom-render.tsx
@@ -1,18 +1,43 @@
 import { PropsWithChildren, ReactElement } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { render, RenderResult, RenderOptions } from '@testing-library/react'
+import { QueryClientProvider, QueryClient } from 'react-query'
 
 import { defaultTheme } from '@tooploox-test/theme'
+import { reactQueryDefaults } from '@tooploox-test/configuration'
 
-function DefaultProviders({ children }: PropsWithChildren<{}>): ReactElement {
+function DefaultThemeProviderWithReactQuery({
+  children,
+}: PropsWithChildren<{}>): ReactElement {
+  const queryClient = new QueryClient({
+    ...reactQueryDefaults,
+  })
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </ThemeProvider>
+  )
+}
+
+function DefaultThemeProvider({
+  children,
+}: PropsWithChildren<{}>): ReactElement {
   return <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>
+}
+
+function renderWithReactQueryContext(
+  ui: ReactElement,
+  options?: RenderOptions
+): RenderResult {
+  return render(ui, { wrapper: DefaultThemeProviderWithReactQuery, ...options })
 }
 
 function renderWithTheme(
   ui: ReactElement,
   options?: RenderOptions
 ): RenderResult {
-  return render(ui, { wrapper: DefaultProviders, ...options })
+  return render(ui, { wrapper: DefaultThemeProvider, ...options })
 }
 
-export { renderWithTheme }
+export { renderWithTheme, renderWithReactQueryContext }
