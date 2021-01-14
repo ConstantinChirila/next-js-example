53469113f058ca5e213133e82f0002c9bb44ea0b
przemek <przemek.kalisiewicz@purplebricks.com>
2021-01-14 19:06:26 +0000

    add a small get keep for missing .env variable

--- next.config.js
+++ next.config.js
@@ -3,3 +3,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 module.exports = withBundleAnalyzer({})
+
+if (!process.env.NEXT_PUBLIC_GITHUB_TOKEN) {
+  throw new Error(
+    'Please remember to supply github API token to your `.env.local` file.'
+  )
+}
